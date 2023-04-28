8fb8d44ba7284108f834ed5dc13df17317dedf2f
unknown <maliksachin298@gmail.com>
2023-04-28 16:32:59 +0530

    changes made

--- Prime.js
+++ Prime.js
@@ -1,14 +1,14 @@
 let num = 27;
-let count = 1;
+let count = 0;
 
-for(let i = 0; i < num; i++) {
+for(let i = 0; i <= num; i++) {
     if(num % i == 0) {
         count++;
     }
 }
 
 if(count == 2) {
-    console.log("Not Prime");
+    console.log(num," Prime");
 } else {
-    console.log("Prime");
+    console.log(num,"Not Prime");
 }
